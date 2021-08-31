8f4e807dbaba41f37c1bebaf759c4b21a2a06618
minseokSong <82006053+Noesis-ms@users.noreply.github.com>
2021-08-31 15:48:43 +0900

    Update app.js
    login 기능 구현
    로그인시 정보 저장기능 구현

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    exing.innerText = `Hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
+}
+
+function paintGreeting(uername){
+    greeting.innerText = `Hello ${uername}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreeting(savedUsername);
+}
