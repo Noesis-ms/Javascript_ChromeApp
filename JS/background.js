d068f0bb9ce84f916c7404b0747aefd54db70ce0
minseokSong <82006053+Noesis-ms@users.noreply.github.com>
2021-09-03 14:29:32 +0900

    Update background.js

--- JS/background.js
+++ JS/background.js
@@ -1,8 +1,11 @@
-const image = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG"];
+const image = ["1.jpg", "2.jpg", "3.jpg"];
+
+const BG = "background";
 
 const chosenImage = image[Math.floor(Math.random() * image.length)];
 
 const bgImage = document.createElement("img");
+bgImage.className = BG;
 
 bgImage.src = `img/${chosenImage}`;
 
