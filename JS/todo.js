86a0dec36605629da22332e0de48cbc080e2f93f
minseokSong <82006053+Noesis-ms@users.noreply.github.com>
2021-09-03 14:29:44 +0900

    Create todo.js

---
+++ JS/todo.js
@@ -0,0 +1,15 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+if (savedUsername === null) {
+  toDoForm.classList.add(HIDDEN_CLASSNAME);
+}
